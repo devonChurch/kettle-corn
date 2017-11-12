8dd6dcff16d63274be3732a9431a9c4a0602de8e
strivegfx <notifydevon@gmail.com>
2017-11-12 13:17:20 +1300

    fix: add async/await support
    add async/await support to Node 7.0.0 for e2e tests with AWS Codebuild

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const isProduction = NODE_ENV === 'production';
 
-const version = 'v2.2.1';
+const version = 'v2.2.2';
 
 const lambdaEndpoint = isProduction
   ? 'https://57dina5fih.execute-api.us-east-1.amazonaws.com/dev'
