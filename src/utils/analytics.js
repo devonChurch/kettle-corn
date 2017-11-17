85cc966b03db5e6620eee89588a4bcec60babad9
strivegfx <notifydevon@gmail.com>
2017-11-17 22:47:12 +1300

    feat: install only GA lib
    install only GA lib

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,14 +1,13 @@
 import config from './config';
 
 const start = () => {
-  const window = window || {};
-  if (config.isProduction && window) {
-    console.log('start analytics');
-    const { dataLayer = [] } = window;
-
-    dataLayer.push('js', new Date());
-    dataLayer.push('config', 'UA-108251862-1');
-  }
+  // const window = window || {};
+  // if (config.isProduction && window) {
+  //   console.log('start analytics');
+  //   const { dataLayer = [] } = window;
+  //   dataLayer.push('js', new Date());
+  //   dataLayer.push('config', 'UA-108251862-1');
+  // }
 };
 
 const analytics = { start };
