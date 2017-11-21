4fdcc20be5f31ee9d5d8e661e32a124a465b243e
strivegfx <notifydevon@gmail.com>
2017-11-22 10:03:04 +1300

    puppeteer testing

--- screenshots.js
+++ screenshots.js
@@ -11,24 +11,30 @@ const screenshots = path.resolve(__dirname, './screenshots');
 
 console.log({ isProduction });
 
-['services', 'contact', 'styleguide'].forEach(async test => {
-  try {
-    console.log(`*** ${test} | start`);
-    const browser = await puppeteer.launch(puppeteerOptions);
-    const page = await browser.newPage();
-    await fs.ensureDir(screenshots);
-    await page.goto(`https://enhancedigital.co.nz/${test}/`, { waitUntil: 'load' });
+// ['services', 'contact', 'styleguide'].forEach(async test => {
+const start = async () => {
+  for (test of ['services', 'contact', 'styleguide']) {
+    try {
+      console.log(`*** ${test} | start`);
+      const browser = await puppeteer.launch(puppeteerOptions);
+      const page = await browser.newPage();
+      await fs.ensureDir(screenshots);
+      await page.goto(`https://enhancedigital.co.nz/${test}/`, { waitUntil: 'load' });
 
-    for (width of [320, 600, 900, 1200]) {
-      console.log(`>>> ${test} | ${width}px`);
-      await page.setViewport({ width, height: 600 });
-      const saveAs = path.resolve(screenshots, `${test}-${width}.png`);
-      const screenshot = await page.screenshot({ fullPage: true, path: saveAs });
+      for (width of [320, 600, 900, 1200]) {
+        console.log(`>>> ${test} | ${width}px`);
+        await page.setViewport({ width, height: 600 });
+        const saveAs = path.resolve(screenshots, `${test}-${width}.png`);
+        const screenshot = await page.screenshot({ fullPage: true, path: saveAs });
+      }
+
+      await browser.close();
+    } catch (error) {
+      console.log(error);
     }
-
-    await browser.close();
-  } catch (error) {
-    console.log(error);
+    console.log(`--- ${test} | finish`);
   }
-  console.log(`--- ${test} | finish`);
-});
+};
+// });
+
+start();
