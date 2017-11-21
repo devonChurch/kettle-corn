ae11b09de90cd8b5410bb7b7987c7fc02cbd953e
strivegfx <notifydevon@gmail.com>
2017-11-22 09:50:30 +1300

    puppeteer testing

--- screenshots.js
+++ screenshots.js
@@ -13,12 +13,14 @@ console.log({ isProduction });
 
 ['services', 'contact', 'styleguide'].forEach(async test => {
   try {
+    console.log(`*** ${test} | start`);
     const browser = await puppeteer.launch(puppeteerOptions);
     const page = await browser.newPage();
     await fs.ensureDir(screenshots);
     await page.goto(`https://enhancedigital.co.nz/${test}/`, { waitUntil: 'load' });
 
     for (width of [320, 600, 900, 1200]) {
+      console.log(`>>> ${test} | ${width}px`);
       await page.setViewport({ width, height: 600 });
       const saveAs = path.resolve(screenshots, `${test}-${width}.png`);
       const screenshot = await page.screenshot({ fullPage: true, path: saveAs });
@@ -28,4 +30,5 @@ console.log({ isProduction });
   } catch (error) {
     console.log(error);
   }
+  console.log(`--- ${test} | finish`);
 });
